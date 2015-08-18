722716f69fd0ab52af1542a648dfe20f89f50a06
Viktor Wase <viktor@Saturnus.precisit.com>
2015-08-18 13:50:46 +0200

    Forgot about this file.

--- CFD/Multigrid FDM/Wavelet and adaptive multigrid/parameters.h
+++ CFD/Multigrid FDM/Wavelet and adaptive multigrid/parameters.h
@@ -13,6 +13,7 @@ const int row = 17;
 const int colum = 17;	
 const int step = 2;
 const int layers = 4;	
+const int LAYERS = 4; //Global variables shouldn't be used, but it they are: use capitals.
 //const int size = row*colum* sizeof(Node);
 //const int size = 80; //FIX!
 const float tol = 0.2;
